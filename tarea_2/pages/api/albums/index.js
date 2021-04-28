cefbcc05a41142dbad4080413bd1df29372cbe70
Maximiliano Stuardo <mestuardo@uc.cl>
2021-04-27 23:41:08 -0400

    CORS

--- tarea_2/pages/api/albums/index.js
+++ tarea_2/pages/api/albums/index.js
@@ -1,6 +1,10 @@
 import dbConnect from '../../../util/mongoose'
 import Album from '../../../models/Album'
 
+import Cors from 'cors'
+import initMiddleware from '../../../util/init-middleware'
+
+
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
