41bf35786cba1454a28c7d35fa941bfa8ccaf67e
Maximiliano Stuardo <mestuardo@uc.cl>
2021-04-27 22:42:25 -0400

    CORS

--- tarea_2/pages/api/artists/index.js
+++ tarea_2/pages/api/artists/index.js
@@ -1,8 +1,21 @@
 import dbConnect from '../../../util/mongoose'
 import Artist from '../../../models/Artist'
 
+import Cors from 'cors'
+import initMiddleware from '../../../util/init-middleware'
+
+
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST'],
+  })
+)
+
 
 export default async function handler(req, res) {  
+  await cors(req, res)
   const { method } = req
 
 
