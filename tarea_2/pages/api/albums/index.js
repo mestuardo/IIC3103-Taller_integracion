26d25bc9415d3348717a3968a8a8e0ffb6c457db
Maximiliano Stuardo <mestuardo@uc.cl>
2021-04-27 23:22:09 -0400

    CORS

--- tarea_2/pages/api/albums/index.js
+++ tarea_2/pages/api/albums/index.js
@@ -1,9 +1,19 @@
 import dbConnect from '../../../util/mongoose'
 import Album from '../../../models/Album'
 
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST'],
+  })
+)
+
 
 export default async function handler(req, res) {  
+  await cors(req, res)
   const { method } = req
+
 
 
   await dbConnect()
